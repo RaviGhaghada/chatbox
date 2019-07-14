1a557ee334e2f6936ade9ebe9fff1f37b952ad07
Ravi <ravighaghada28@gmail.com>
2019-07-14 18:37:59 +0100

    Most basic functionality added
    + Multiple clients can communicate with each other via a server
    + Currently all communication is anonymous to the user
    + Upgraded it from a version where a server can talk to only only the client

---
+++ client/ClientTest.java
@@ -0,0 +1,12 @@
+package client;
+
+import javax.swing.*;
+
+public class ClientTest {
+
+    public static void main(String[] args) {
+        Client client = new Client("127.0.0.1");
+        client.startRunning();
+        client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
